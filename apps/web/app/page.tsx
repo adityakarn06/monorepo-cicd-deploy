41158d645371a36c6547ebd6c0e523c14d4a08cc
Aditya Raj <adityakarn06@gmail.com>
2025-07-20 01:03:00 +0530

    added_dockerfiles

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,3 +9,12 @@ export default async function Home() {
     </div>
   );
 }
+
+// by default, Next.js will statically generate this page at build time (and have to talk to db at build time here).
+// if you want to enable 'force-dynamic' (then it doesn't need to talk to db at build time bcs it will not generate statically).
+// or you can export `revalidate` to enable incremental static regeneration
+// incremental static regeneration (ISR) can be enabled by uncommenting the line below
+// export const revalidate = 60 // revalidate every 60 seconds
+// or
+// to force dynamic rendering, uncomment the line below
+// export const dynamic = 'force-dynamic'
